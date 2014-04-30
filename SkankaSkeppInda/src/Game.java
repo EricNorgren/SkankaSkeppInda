ff32c519c42db3fbdeb43777cd7743b4e070db5f
guest <guest@TP>
2014-04-30 13:35:12 +0200

    skellett

--- SkankaSkeppInda/src/Game.java
+++ SkankaSkeppInda/src/Game.java
@@ -1,4 +1,24 @@
 
 public class Game {
-	//todo!
+	private static boolean endGame;
+	
+	public static void main(String[] args) {
+		//lägg till allt som skall läggas till
+		
+		//initieringsmetoder?
+		
+		
+		while ( endGame == false ){
+			//loopa igenom själva spelen
+			
+			
+			//några ifsatser antar jag
+			
+			
+			// Dra ner info från player, board, boat
+			
+			
+			//Anropa GUI med infon vi fick där upp
+		}
+	}
 }
