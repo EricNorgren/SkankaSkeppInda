ad26730cd915530ae8a2bd49ebdf1b1bc33c3208
guest <guest@TP>
2014-04-30 15:32:24 +0200

    a

--- SkankaSkeppInda/src/GUI.java
+++ SkankaSkeppInda/src/GUI.java
@@ -1,4 +1,23 @@
 
-public class GUI extends Game {
-//Dafuq?
+public class GUI {
+
+	/*public GUI(Player players, int[][] boats){
+		//Jag tänkte du kan väl lagra varsin pekare till boats och players
+		//här eftersom dessa ändras inte under spelet
+	}*/
+
+	public GUI(){
+	}
+	
+
+	
+	public static int[][] drawPutBoats(){
+		//här måste jag veta var båtarna läggs!
+		return null;
+	}
+	public void draw(int[][] bombs){
+		
+	}
+
+
 }
--- SkankaSkeppInda/src/Game.java
+++ SkankaSkeppInda/src/Game.java
@@ -2,13 +2,33 @@
 public class Game {
 	private static boolean endGame;
 	private static int[] hits;
+	private static int[][] bombs;
+	private static int[][] boats;
+	//private static int[][] bombs;
 	private static Player players;
+	private static String[] names;
+	//private Board boards;
+	//private Boat boats;
+	private GUI gui;
 	
 	public static void main(String[] args) {
 		//lägg till allt som skall läggas till
 		
 		//initieringsmetoder?
+		
 		//metod Skapa spelare
+		
+		//metod lägg ut båtar
+		
+		//GUI gui;
+		
+		gui = new GUI();
+		
+		
+		
+		boats = GUI.drawPutBoats();
+		
+		
 		
 		while ( endGame == false ){
 			//loopa igenom själva spelen
@@ -17,16 +37,15 @@ public class Game {
 			//några ifsatser antar jag
 			
 			
-			// Dra ner info från player, board, boat
-			
-			
+			// Dra ner info från 
 			//Anropa GUI med infon vi fick där upp
+			gui.draw(bombs);
 			
 			
 			
 			
 			endGame = checkEndGame();
-			}
+		}
 	}
 	
 	private static boolean checkEndGame(){
@@ -34,6 +53,9 @@ public class Game {
 		return false;
 	}
 	
-	
+	private static void putBoats(){
+		
+		//return false;
+	}
 	
 }
